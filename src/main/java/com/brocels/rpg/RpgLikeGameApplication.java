53895ad27512fdf4c15ad45cf7cb49fb61e97236
Brocel <geenens.prof@gmail.com>
2021-07-28 11:12:25 -0300

    First commit

---
+++ src/main/java/com/brocels/rpg/RpgLikeGameApplication.java
@@ -0,0 +1,13 @@
+package com.brocels.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgLikeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgLikeGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brocels/rpg/RpgLikeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocels.rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgLikeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
