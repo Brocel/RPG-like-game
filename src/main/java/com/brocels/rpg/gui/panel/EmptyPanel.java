06859c84c179927b9a52bf1773a773560bb000f4
Brocel <geenens.prof@gmail.com>
2021-07-28 14:20:27 -0300

    edit empty panel

--- src/main/java/com/brocels/rpg/gui/panel/EmptyPanel.java
+++ src/main/java/com/brocels/rpg/gui/panel/EmptyPanel.java
@@ -1,5 +1,22 @@
 package com.brocels.rpg.gui.panel;
 
-public class EmptyPanel {
+import javax.swing.JPanel;
+import java.awt.*;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmptyPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public EmptyPanel() {
+		this.setPreferredSize(new Dimension(500, 300));
+		this.setBackground(Color.BLACK);
+		this.setLayout(new GridLayout(2,1));
+	}
 
 }
